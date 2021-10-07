170a0910c903baa2d89aa5692be38d0fe86ff6bf
seongjinme <hongseongjin.to@gmail.com>
2021-10-07 10:24:09 +0900

    Added new functions for initializing user, post, category db

--- blog/checker.py
+++ blog/checker.py
@@ -1,5 +1,42 @@
-from db import get_db
+from blog.db import get_db
 import re
+
+
+def check_user_exists():
+    if get_db().execute('SELECT userid FROM user').fetchone() is None:
+        return False
+    return True
+
+
+def check_category_exists():
+    db = get_db()
+
+    # If there's no category, create default category named "Uncategorized"
+    if db.execute('SELECT id FROM category').fetchone() is None:
+        db.execute(
+            'INSERT INTO category (name, slug) VALUES (?, ?)',
+            ('Uncategorized', 'uncategorized')
+        )
+        db.commit()
+
+
+def check_post_exists(userid):
+    db = get_db()
+
+    # If there's no post after new userid and category have created, create default one
+    if db.execute('SELECT id FROM post').fetchone() is None:
+        user = db.execute('SELECT id FROM user WHERE userid = ?', (userid,)).fetchone()
+        category = db.execute('SELECT id FROM category WHERE name = ?', ('Uncategorized',)).fetchone()
+
+        # If the default category ('Uncategorized') doesn't exists, choose the oldest category
+        if category is None:
+            category = db.execute('SELECT id FROM category ORDER BY id').fetchone()
+        db.execute(
+            'INSERT INTO post (user_id, category_id, title, slug, body) VALUES (?, ?, ?, ?, ?)',
+            (user['id'], category['id'], 'Hello, world!', 'hello-world',
+             "Thank you for using tiny-blogger! Click 'Admin' > 'New Post' to create your own posts :)")
+        )
+        db.commit()
 
 
 def check_slug_valid(slug):
@@ -11,7 +48,7 @@ def check_slug_valid(slug):
     return True
 
 
-def check_slug_duplicated(post_id, slug):
+def check_slug_not_duplicated(post_id, slug):
     db = get_db()
     count = db.execute(
         'SELECT id, slug FROM post WHERE slug = ?',
@@ -22,7 +59,18 @@ def check_slug_duplicated(post_id, slug):
     return False
 
 
-def check_input_error(title, slug, body, post_id):
+def check_category_valid(category_id):
+    db = get_db()
+    category = db.execute(
+        'SELECT id, name FROM category WHERE id = ?',
+        (category_id,)
+    ).fetchone()
+    if category is None:
+        return False
+    return True
+
+
+def check_input_error(title, slug, body, post_id, category_id):
     error = None
     if not title:
         error = 'Title is required. (Up to 100 characters)'
@@ -30,8 +78,10 @@ def check_input_error(title, slug, body, post_id):
         error = 'Post URL Slug is required. (Up to 200 characters)'
     elif not check_slug_valid(slug):
         error = 'Blank spaces, special chars except hyphen(-) are not allowed for Post URL slug. (Up to 200 characters)'
+    elif not check_category_valid(category_id):
+        error = 'Valid category must be selected.'
     elif post_id is not None:
-        if not check_slug_duplicated(post_id, slug):
+        if not check_slug_not_duplicated(post_id, slug):
             error = 'Post URL Slug is duplicated.'
     elif not body:
         error = 'Body is required.'
