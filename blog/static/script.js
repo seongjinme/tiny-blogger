17416ed9d89d6824191bc8cb344ac87abac37f49
seongjinme <hongseongjin.to@gmail.com>
2021-10-22 07:40:13 +0900

    Reconstructed code structure to be worked as a module

--- blog/static/script.js
+++ blog/static/script.js
@@ -1,42 +1,26 @@
-create.addEventListener('input', enable_submit);
-
-title.addEventListener('change', () => {
-    slug.value = slugify(title.value);
-    update_post_url();
-    btn_edit_slug.disabled = !slug.value;
-    enable_submit();
-});
-
-slug.addEventListener('change', update_post_url)
-btn_edit_slug.addEventListener('click', btn_edit_slug_action);
-
-function update_post_url() {
-    document.getElementById('post_slug').textContent = slug.value + '/';
-}
-
-function btn_edit_slug_action() {
+export function btn_edit_slug_action(context, title, slug, slug_url, btn_edit_slug, slug_length_max, btn_submit) {
     if (slug.readOnly) {
+        slug.readOnly = false;
         btn_edit_slug.textContent = 'Done';
         btn_edit_slug.className = 'btn btn-success';
-        slug.readOnly = false;
     } else {
+        slug.readOnly = true;
         btn_edit_slug.textContent = 'Edit';
         btn_edit_slug.className = 'btn btn-primary';
-        slug.readOnly = true;
-        fill_slug();
+        fill_slug(slug, title, slug_length_max);
+        slug_url.textContent = slug.value;
     }
-    enable_submit();
+    enable_submit(title, slug, context, btn_submit);
 }
 
-function fill_slug() {
+export function fill_slug(slug, title, slug_length_max) {
     slug.value = slugify(slug.value);
     if (!slug.value) {
-        slug.value = slugify(title.value);
+        slug.value = slugify(title.value, slug_length_max);
     }
-    update_post_url();
 }
 
-function slugify(text) {
+export function slugify(text, slug_length_max) {
     // This codes are applied version of the character conversion script in Ghost CMS, made by Ghost Foundation Ltd.
     // https://github.com/TryGhost/Ghost
 
@@ -62,12 +46,29 @@ function slugify(text) {
     text = text.trim();
 
     // Limit length of URL slug to 200 characters
-    text = text.substr(0, 200)
+    text = text.substr(0, slug_length_max);
 
     return text;
 }
 
-function enable_submit() {
-    content = document.querySelector('div.note-editable.card-block');
-    submit.disabled = !(title.value && slug.readOnly && slug.value && category.options[category.selectedIndex].value && (content.innerHTML != '<p><br></p>' && content.innerHTML != ''));
+export function enable_submit(title, slug, context, btn_submit) {
+    if (context == 'category') {
+        btn_submit.disabled = !(title.value && slug.readOnly && slug.value);
+    }
+    else if (context == 'post') {
+        let content = document.querySelector('div.note-editable.card-block');
+        btn_submit.disabled = !(title.value && slug.readOnly && slug.value && category.options[category.selectedIndex].value && (content.innerHTML != '<p><br></p>' && content.innerHTML != ''));
+    }
+}
+
+export function cancel_submit(title, slug, slug_url, context, btn_edit_slug) {
+    if (context == 'category') {
+        title.value = '';
+        slug.value = '';
+        slug_url.textContent = '';
+        btn_edit_slug.textContent = 'Edit';
+        btn_edit_slug.className = 'btn btn-primary';
+        btn_edit_slug.disabled = true;
+        slug.readOnly = true;
+    }
 }
