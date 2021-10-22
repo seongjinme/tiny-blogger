0d9909fb344251731339ba50767b3870f25a810e
seongjinme <hongseongjin.to@gmail.com>
2021-10-23 01:38:00 +0900

    Fixed validation codes for registration and login

--- blog/auth.py
+++ blog/auth.py
@@ -4,7 +4,9 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from blog.db import get_db
-from blog.checker import check_category_exists, check_post_exists
+from blog.checker import (
+    check_category_exists, check_post_exists, check_setting_exists, check_account_registration_valid, check_user_exists
+)
 from blog.getter import get_blog_info
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -13,58 +15,70 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        userid = request.form['userid']
-        username = request.form['username']
-        password = request.form['password']
+        values = {
+            'blog_title': request.form['blog_title'],
+            'userid': request.form['userid'],
+            'username': request.form['username'],
+            'password': request.form['password'],
+            'password_confirm': request.form['password_confirm']
+        }
         db = get_db()
-        error = None
 
         # Validate input data
-        if not userid:
-            error = 'User ID is required.'
-        elif not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
+        error = check_account_registration_valid(values)
 
         # Insert input data to DB
-        if error is None:
+        if error is not None:
+            flash(error)
+
+        else:
             try:
                 db.execute(
                     'INSERT INTO user (userid, username, password) VALUES (?, ?, ?)',
-                    (userid, username, generate_password_hash(password)),
+                    (values['userid'], values['username'], generate_password_hash(values['password'])),
                 )
                 db.commit()
 
             # Check if userid is duplicated
             except db.IntegrityError:
-                error = f'User ID {userid} is already registered.'
+                error = f"User ID {values['userid']} is already registered."
 
             # If registration is completed, redirect to login page
             else:
+
+                # Check if there's any setting values, and insert default values
+                check_setting_exists()
 
                 # Check if there's any category exists, and create one if there isn't
                 check_category_exists()
 
                 # Check and create the first post
-                check_post_exists(userid)
+                check_post_exists(values['userid'])
+
+                # Update blog title into db
+                db.execute('UPDATE setting SET blog_title = ?', (values['blog_title'],))
+                db.commit()
+
+                flash('Your registration has successfully completed!')
 
                 return redirect(url_for('auth.login'))
-
-        # Store error message
-        flash(error)
 
     # If registered user exists when someone accesses register page with 'GET' method, redirect to index
     if get_db().execute('SELECT userid FROM user').fetchone() is not None:
         error = 'Registered user account already exists.'
         flash(error)
-        return redirect(url_for("blog.index"))
+        return redirect(url_for('blog.index'))
 
-    return render_template('auth/register.html')
+    return render_template('auth/register.html', alarm_type='danger')
 
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    error = None
+    # If there's no registered user, redirect to register page
+    if not check_user_exists():
+        return redirect(url_for('auth.register'))
+
     if session.get('user_id'):
         flash("You're already logged in!")
         return redirect(url_for('blog.index'))
@@ -72,7 +86,6 @@ def login():
     if request.method == 'POST':
         userid = request.form['userid']
         password = request.form['password']
-        error = None
 
         # Ensure userid and password is submitted
         if not userid:
@@ -97,9 +110,11 @@ def login():
 
         # Store error message
         flash(error)
-        return render_template('auth/login.html', blog_info=get_blog_info(), alarm_type='light')
 
-    return render_template('auth/login.html', blog_info=get_blog_info())
+    if error:
+        return render_template('auth/login.html', blog_info=get_blog_info(), alarm_type='danger')
+
+    return render_template('auth/login.html', blog_info=get_blog_info(), alarm_type='success')
 
 
 # Check user info of current session before view function in blog
